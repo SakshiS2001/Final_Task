a4a2788dd298184e9aac4ab99c74e592ab0f6a16
SakshiS2001 <sakshisawantj@gmail.com>
2024-12-31 19:22:08 +0530

    terraform updateded

--- terraform_setup.sh
+++ terraform_setup.sh
@@ -1,10 +1,11 @@
+
 #!/bin/bash
 
 # Terraform
-sudo apt install gnupg gpg
+apt install gnupg gpg
 wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | 
 
-sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform -y
+tee /etc/apt/sources.list.d/hashicorp.list
+apt update && apt install terraform -y
 
